c2d2e93d842d3845e3c2a1806d90047af602f417
PogorzelskiJakub <pogorzelskijakub@wp.pl>
2024-07-03 14:55:12 +0200

    recykling

---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+require("session.php");
+require("db.php");
+?>
+<nav>
+    <a href="index.php">Kalendarz</a>
+    <a href="list.php">Lista</a>
+    <a href="followed.php">Obserwowane</a>
+    <a href="mySuggestions.php">Moje propozycje</a>
+    <a href="logout.php">Wyloguj</a>
+</nav>
