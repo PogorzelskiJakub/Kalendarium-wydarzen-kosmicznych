2e56dd613c7dd50e7da80f202835caa9bd0c9285
PogorzelskiJakub <pogorzelskijakub@wp.pl>
2024-07-03 15:14:54 +0200

    menu dla zalogowanych

--- menu.php
+++ menu.php
@@ -5,7 +5,11 @@ require("db.php");
 <nav>
     <a href="index.php">Kalendarz</a>
     <a href="list.php">Lista</a>
-    <a href="followed.php">Obserwowane</a>
-    <a href="mySuggestions.php">Moje propozycje</a>
-    <a href="logout.php">Wyloguj</a>
+    <?php if(isset($_SESSION["login"])): ?>
+        <a href="followed.php">Obserwowane</a>
+        <a href="mySuggestions.php">Moje propozycje</a>
+        <a href="logout.php">Wyloguj</a>
+    <?php else: ?>
+        <a href="login.php">Zaloguj</a>
+    <?php endif; ?>
 </nav>
